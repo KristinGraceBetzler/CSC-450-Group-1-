45737ae4e5348b3fc7b9a9840d391baf1c31bb68
Cody Zellmer-Johnson <114022227+DeadIgnite@users.noreply.github.com>
2024-02-08 15:39:41 -0600

    Added base Spring Boot project via upload

---
+++ src/main/java/com/github/CSC450Group1/wefli/WefliApplication.java
@@ -0,0 +1,13 @@
+package com.github.CSC450Group1.wefli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WefliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/CSC450Group1/wefli/WefliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.CSC450Group1.wefli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
