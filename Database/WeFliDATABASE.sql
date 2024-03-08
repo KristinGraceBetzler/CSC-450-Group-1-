1d2f4a7e30e5232c3804427706a7e98f1159d3fd
KristinGraceBetzler <152664199+KristinGraceBetzler@users.noreply.github.com>
2024-03-08 10:52:57 -0600

    Update WeFliDATABASE.sql
    Updated the naming to all lowercase for Cody's code. Added under excursion excursion_type so that we are able to pull different excursions for the quiz result.

--- Database/WeFliDATABASE.sql
+++ Database/WeFliDATABASE.sql
@@ -3,100 +3,100 @@ CREATE DATABASE IF NOT EXISTS WeFli;
 USE WeFli;
 
 -- Create User Table
-CREATE TABLE IF NOT EXISTS User (
-    User_ID INT PRIMARY KEY,
-    First_Name VARCHAR(100),
-    Last_Name VARCHAR(100),
-    Phone_Number VARCHAR(100),
-    Email VARCHAR(100),
-    Address VARCHAR(100),
-    User_Name VARCHAR(100),
-    Password VARCHAR(100),
-    Verification_Code INT,
-    Verified TINYINT(1) NOT NULL
+CREATE TABLE IF NOT EXISTS user (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    phone_number VARCHAR(100),
+    email VARCHAR(100),
+    address VARCHAR(100),
+    user_name VARCHAR(100),
+    password VARCHAR(100),
+    verification_code INT,
+    verified TINYINT(1) NOT NULL
 );
 
 -- Create Comments Table
-CREATE TABLE IF NOT EXISTS Comments (
-    Comment_ID INT PRIMARY KEY,
-    User_ID INT,
-    Comment VARCHAR(100),
-    FOREIGN KEY (User_ID) REFERENCES User(User_ID)
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id INT PRIMARY KEY,
+    user_id INT,
+    comment VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 -- Create User_Content Table
-CREATE TABLE IF NOT EXISTS User_Content (
-    User_Content_ID INT PRIMARY KEY,
-    User_ID INT,
-    User_Post VARCHAR(100),
-    Comment_ID INT,
-    FOREIGN KEY (User_ID) REFERENCES User(User_ID),
-    FOREIGN KEY (Comment_ID) REFERENCES Comments(Comment_ID)
+CREATE TABLE IF NOT EXISTS user_content (
+    user_content_id INT PRIMARY KEY,
+    user_id INT,
+    user_post VARCHAR(100),
+    comment_id INT,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id)
 );
 
 -- Create Quiz Table
-CREATE TABLE IF NOT EXISTS Quiz (
-    Quiz_ID INT PRIMARY KEY,
-    Quiz_Question VARCHAR(100),
-    Quiz_Option_1 VARCHAR(100),
-    Quiz_Option_2 VARCHAR(100)
+CREATE TABLE IF NOT EXISTS quiz (
+    quiz_id INT PRIMARY KEY,
+    quiz_question VARCHAR(100),
+    quiz_option_1 VARCHAR(100),
+    quiz_option_2 VARCHAR(100)
 );
 
 -- Create Hotels Table
-CREATE TABLE IF NOT EXISTS Hotels (
-    Hotel_ID INT PRIMARY KEY,
-    Hotel_Name VARCHAR(100),
-    Check_In_Date DATETIME,
-    Check_Out_Date DATETIME,
-    Check_In_Time VARCHAR(100),
-    Check_Out_Time VARCHAR(100),
-    Room_Type VARCHAR(100),
-    Hotel_Type VARCHAR(100)
+CREATE TABLE IF NOT EXISTS hotels (
+    hotel_id INT PRIMARY KEY,
+    hotel_name VARCHAR(100),
+    check_in_date DATETIME,
+    check_out_date DATETIME,
+    check_in_time VARCHAR(100),
+    check_out_time VARCHAR(100),
+    room_type VARCHAR(100),
+    hotel_type VARCHAR(100)
 );
 
 -- Create Flight Table
-CREATE TABLE IF NOT EXISTS Flight (
-    Flight_ID INT PRIMARY KEY,
-    Departure_Airport VARCHAR(100),
-    Destination_Airport VARCHAR(100),
-    Departure_Date DATETIME,
-    Arrival_Date DATETIME,
-    Departure_Time VARCHAR(100),
-    Arrival_Time VARCHAR(100),
-    Airline_Name VARCHAR(100),
-    Flight_Time VARCHAR(100)
+CREATE TABLE IF NOT EXISTS flight (
+    flight_id INT PRIMARY KEY,
+    departure_airport VARCHAR(100),
+    destination_airport VARCHAR(100),
+    departure_date DATETIME,
+    arrival_date DATETIME,
+    departure_time VARCHAR(100),
+    arrival_time VARCHAR(100),
+    airline_name VARCHAR(100),
+    flight_time VARCHAR(100)
 );
 
 -- Create Destinations Table
-CREATE TABLE IF NOT EXISTS Destinations (
-    Destination_ID INT PRIMARY KEY,
-    Destination_State VARCHAR(100),
-    Destination_City VARCHAR(100),
-    DestinationClimate VARCHAR(100)
+CREATE TABLE IF NOT EXISTS destinations (
+    destination_id INT PRIMARY KEY,
+    destination_state VARCHAR(100),
+    destination_city VARCHAR(100),
+    destination_climate VARCHAR(100)
 );
 
 -- Create Trip Table
-CREATE TABLE IF NOT EXISTS Trip (
-    Trip_ID INT PRIMARY KEY,
-    User_ID INT,
-    Flight_ID INT,
-    Hotel_ID INT,
-    Destination_ID INT,
-    Comment_ID INT,
-    FOREIGN KEY (User_ID) REFERENCES User(User_ID),
-    FOREIGN KEY (Flight_ID) REFERENCES Flight(Flight_ID),
-    FOREIGN KEY (Hotel_ID) REFERENCES Hotels(Hotel_ID),
-    FOREIGN KEY (Destination_ID) REFERENCES Destinations(Destination_ID),
-    FOREIGN KEY (Comment_ID) REFERENCES Comments(Comment_ID)
+CREATE TABLE IF NOT EXISTS trip (
+    trip_id INT PRIMARY KEY,
+    user_id INT,
+    flight_id INT,
+    hotel_id INT,
+    destination_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (flight_id) REFERENCES flight(flight_id),
+    FOREIGN KEY (hotel_id) REFERENCES hotels(hotel_id),
+    FOREIGN KEY (destination_id) REFERENCES destinations(destination_id),
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id)
 );
 
 -- Create Excursions Table 
-CREATE TABLE IF NOT EXISTS Excursions(
-	Excursion_ID INT PRIMARY KEY,
-    Excursion_Title VARCHAR(100),
-    Excursion_Description VARCHAR(100),
-    Destination_ID INT,
-    FOREIGN KEY (Destination_ID) REFERENCES Destinations(Destination_ID)
+CREATE TABLE IF NOT EXISTS excursions(
+    excursion_id INT PRIMARY KEY,
+    excursion_title VARCHAR(100),
+    excursion_description VARCHAR(100),
+    type_of_excursion VARCHAR(100),
+    destination_id INT,
+    FOREIGN KEY (destination_id) REFERENCES destinations(destination_id)
 );
     
     
