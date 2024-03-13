f5dbe6e8dc9fe727b95674231bd033b8fa497573
KristinGraceBetzler <152664199+KristinGraceBetzler@users.noreply.github.com>
2024-03-12 20:45:07 -0500

    Update WeFliDATABASE.sql
    Got rid of the flights table and hotels table and made small changes.

--- Database/WeFliDATABASE.sql
+++ Database/WeFliDATABASE.sql
@@ -5,7 +5,7 @@ USE WeFli;
 
 -- Create User Table
 CREATE TABLE IF NOT EXISTS user (
-    user_id INT PRIMARY KEY,
+    userID INT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     phone_number VARCHAR(100),
@@ -170,60 +170,21 @@ INSERT INTO comments (comment_id, user_id, comment) VALUES
     (555555647, 111111143, 'The world is full of wonders – go and explore them all!'),
     (555555648, 111111144, 'Wherever you go, may your heart find peace and your soul find joy.');
 
-    
--- Create Quiz Table
-CREATE TABLE IF NOT EXISTS quiz (
-    quiz_id INT PRIMARY KEY,
-    quiz_question VARCHAR(100),
-    quiz_option_1 VARCHAR(100),
-    quiz_option_2 VARCHAR(100)
-);
-
--- Create Hotels Table
-CREATE TABLE IF NOT EXISTS hotels (
-    hotel_id INT PRIMARY KEY,
-    hotel_name VARCHAR(100),
-    check_in_date DATETIME,
-    check_out_date DATETIME,
-    check_in_time VARCHAR(100),
-    check_out_time VARCHAR(100),
-    room_type VARCHAR(100),
-    hotel_type VARCHAR(100)
-);
-
--- Create Flight Table
-CREATE TABLE IF NOT EXISTS flight (
-    flight_id INT PRIMARY KEY,
-    departure_airport VARCHAR(100),
-    destination_airport VARCHAR(100),
-    departure_date DATETIME,
-    arrival_date DATETIME,
-    departure_time VARCHAR(100),
-    arrival_time VARCHAR(100),
-    airline_name VARCHAR(100),
-    flight_time VARCHAR(100)
-);
-
 -- Create Destinations Table
 CREATE TABLE IF NOT EXISTS destinations (
     destination_id INT PRIMARY KEY,
     destination_state VARCHAR(100),
     destination_city VARCHAR(100),
-    destination_climate VARCHAR(100)
+    destination_climate_tag VARCHAR(100)
 );
 
 -- Create Trip Table
 CREATE TABLE IF NOT EXISTS trip (
     trip_id INT PRIMARY KEY,
     user_id INT,
-    flight_id INT,
-    hotel_id INT,
     destination_id INT,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (flight_id) REFERENCES flight(flight_id),
-    FOREIGN KEY (hotel_id) REFERENCES hotels(hotel_id),
     FOREIGN KEY (destination_id) REFERENCES destinations(destination_id),
-    FOREIGN KEY (comment_id) REFERENCES comments(comment_id)
 );
 
 -- Create Excursions Table 
@@ -231,7 +192,7 @@ CREATE TABLE IF NOT EXISTS excursions(
     excursion_id INT PRIMARY KEY,
     excursion_title VARCHAR(100),
     excursion_description VARCHAR(100),
-    type_of_excursion VARCHAR(100),
+    type_of_excursion_tag VARCHAR(100),
     destination_id INT,
     FOREIGN KEY (destination_id) REFERENCES destinations(destination_id)
 );
