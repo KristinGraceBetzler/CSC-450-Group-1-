566503bfe260ee94fb1e831731eb0846dd9afee4
Cody Zellmer-Johnson <114022227+DeadIgnite@users.noreply.github.com>
2024-04-16 15:54:21 -0500

    Added endpoint to retrieve comments for a post
    Added endpoint that allows the front end to get all the comments on a post using the tripID. Updated the endpoints documentation page.

--- src/main/java/com/github/CSC450Group1/wefli/Trip/Repositries/CommentsRepository.java
+++ src/main/java/com/github/CSC450Group1/wefli/Trip/Repositries/CommentsRepository.java
@@ -4,7 +4,9 @@ import com.github.CSC450Group1.wefli.Trip.TripObjects.Comments;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface CommentsRepository extends CrudRepository<Comments, Integer> {
-
+    ArrayList<Comments> findByTripID(int tripID);
 }
